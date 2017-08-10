9a766fbcdb5070f977f395f1f1fa5ef92c2585e2
Humbertzhang <504490160@qq.com>
2017-08-10 22:35:36 +0800

    getname api OK

--- Demo/app/api/demo.py
+++ Demo/app/api/demo.py
@@ -1,9 +1,16 @@
 from . import api
-from guisheng_app import db
+from app import db
 from flask import request,jsonify,Response
 from flask_login import login_user, logout_user, current_user, login_required
-from guisheng_app.models import User
-from guisheng_app.decorators import admin_required
+from app.models import User
 import json
 
 @api.route('/getname/<int:id>',methods=['GET'])
+def getname(id):
+    if request.method == 'GET':
+        uid = id
+        if User.query.filter_by(id=uid).first():
+            user = User.query.filter_by(id=uid).first()
+            return jsonify({
+                        "username":user.username
+                    })
